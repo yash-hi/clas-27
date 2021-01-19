f4b735395332a716a82f03056a901c5afef46efd
yash-hi <75252843+yash-hi@users.noreply.github.com>
2021-01-19 19:22:20 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,23 @@
+class Chain{
+  constructor(bodyA,bodyB){
+    var options ={
+        bodyA:bird.body,
+        bodyB:log6.body,
+        stiffness:0.04,
+        length:10
+    }
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+  }
+    display(){
+    var pointA=this.chain.bodyA.position;
+    var pointB=this.chain.bodyB.position;
+    strokeWeight(3);
+    line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+
+
+
+
+
+}
